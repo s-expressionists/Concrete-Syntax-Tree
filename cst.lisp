d405d9a28b8d4c6f97a09a85d808739f0e4abbff
Robert Strandh <robert.strandh@gmail.com>
2017-03-26 10:44:11 +0200

    Define generic function CONSP and its standard methods.

--- cst.lisp
+++ cst.lisp
@@ -8,7 +8,15 @@
 
 (defgeneric null (cst))
 
+;;; Return true if and only if CST represents an atomic expression.
+;;; Notice that (ATOM CST) is NOT equivalent to (NOT (CONSP CST))
+;;; because there are CSTs that represent neither atoms nor conses.
 (defgeneric atom (cst))
+
+;;; Return true if and only if CST represents a CONS expression.
+;;; Notice that (CONSP CST) is NOT equivalent to (NOT (ATOM CST))
+;;; because there are CSTs that represent neither atoms nor conses.
+(defgeneric consp (cst))
 
 (defclass cst ()
   (;; This slot contains either another CST, namely the parent of this
@@ -24,6 +32,10 @@
   (declare (ignorable cst))
   nil)
 
+(defmethod consp ((cst cst))
+  (declare (ignorable cst))
+  nil)
+
 ;;; This class is used as a terminator of a chain of CSTs.  It does
 ;;; NOT represent NIL in other situations.
 (defclass null-cst (cst) ())
@@ -35,12 +47,14 @@
 (defclass expression-cst (cst)
   ())
 
+;;; This class is used to represent expressions that are atoms.  It is
+;;; not used to represent the end of a chain of CSTs.
 (defclass atom-cst (expression-cst)
   ())
 
 (defmethod atom ((cst atom-cst))
   (declare (ignorable cst))
-  nil)
+  t)
 
 (defclass cons-cst (expression-cst)
   (;; This slot contains a CST that represents the CAR of the
@@ -49,3 +63,7 @@
    ;; This slot contains a CST that represents the CDR of the
    ;; corresponding expression.
    (%rest :initform nil :initarg :rest :reader rest)))
+
+(defmethod consp ((cst cons-cst))
+  (declare (ignorable cst))
+  t)
