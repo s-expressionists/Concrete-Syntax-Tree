154f4ea6a1b9eb7f7653689298f2f205bed3c518
Robert Strandh <robert.strandh@gmail.com>
2017-03-26 09:56:31 +0200

    Shadow and export symbol named ATOM.

--- packages.lisp
+++ packages.lisp
@@ -15,6 +15,7 @@
            #:tenth
            #:nth
            #:rest
+           #:atom
            #:consp
            #:null)
   (:export #:cst
@@ -31,5 +32,6 @@
            #:tenth
            #:nth
            #:rest
+           #:atom
            #:consp
            #:null))
