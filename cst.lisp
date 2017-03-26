e30aab41a8cfdb7b0924595adb59265ef1b79daa
Robert Strandh <robert.strandh@gmail.com>
2017-03-26 09:50:45 +0200

    Introduce class NULL-CST and generic function NULL.

--- cst.lisp
+++ cst.lisp
@@ -6,11 +6,25 @@
 
 (defgeneric rest (cst))
 
+(defgeneric null (cst))
+
 (defclass cst ()
   (;; This slot contains either another CST, namely the parent of this
    ;; one, or, if this is a top-level CST, an indication of the source
    ;; document (a file or an editor buffer or something similar).
    (%parent :initarg :parent :accessor parent)))
+
+(defmethod null ((cst cst))
+  (declare (ignorable cst))
+  nil)
+
+;;; This class is used as a terminator of a chain of CSTs.  It does
+;;; NOT represent NIL in other situations.
+(defclass null-cst (cst) ())
+
+(defmethod null ((cst null-cst))
+  (declare (ignorable cst))
+  t)
 
 (defclass expression-cst (cst)
   (;; If this CST represents an atom, then this slot contains NIL.
