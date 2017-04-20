07047e18eb75daeca8b8b22754b30ff221ddac0d
Robert Strandh <robert.strandh@gmail.com>
2017-04-20 13:09:41 +0200

    Define condition EXPRESSION-CST-REQUIRED.

--- conditions.lisp
+++ conditions.lisp
@@ -2,3 +2,8 @@
 
 (define-condition cst-error (error acclimation:condition)
   ())
+
+;;; This condition is signaled whenever an instance of the class
+;;; CST-EXPRESSION was required, but something else was given.
+(define-condition expression-cst-required (cst-error)
+  ((%cst :initarg :cst :reader cst)))
