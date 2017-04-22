f39975b85c59c8304698922c7bc494807e5f27a9
Robert Strandh <robert.strandh@gmail.com>
2017-04-22 10:41:31 +0200

    Define generic function CANONICALIZE-DECLARATION-SPECIFIER.

--- declarations.lisp
+++ declarations.lisp
@@ -1,2 +1,7 @@
 (cl:in-package #:concrete-syntax-tree)
 
+(defgeneric canonicalize-declaration-specifier
+    (system
+     declaration-identifier
+     declaration-identifier-cst
+     declaration-data))
