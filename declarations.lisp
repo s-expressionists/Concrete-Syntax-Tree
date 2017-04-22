0ceff60c072f35318353234100fd572612903f0f
Robert Strandh <robert.strandh@gmail.com>
2017-04-22 10:46:35 +0200

    Define method on CANONICALIZE-DECLARATION-SPECIFIER for OPTIMIZE.

--- declarations.lisp
+++ declarations.lisp
@@ -6,3 +6,12 @@
 (defmethod  canonicalize-declaration-specifier
     (system declaration-identifier declaration-identifier-cst declaration-data)
   '())
+
+(defmethod  canonicalize-declaration-specifier
+    (system
+     (declaration-identifier (eql 'optimize))
+     declaration-identifier-cst
+     declaration-data)
+  (loop for data = declaration-data then (rest data)
+        until (null data)
+        collect (list declaration-identifier data)))
