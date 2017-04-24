e69e7e20bd33a53fc43036e5ee3f71d6e57c96a9
Robert Strandh <robert.strandh@gmail.com>
2017-04-24 10:31:37 +0200

    Use new auxiliary function in method on OPTIMIZE.

--- declarations.lisp
+++ declarations.lisp
@@ -16,12 +16,9 @@
         until (null remaining)
         collect (list prefix (first remaining))))
 
-
 (defmethod  canonicalize-declaration-specifier
     (system
      (declaration-identifier (eql 'optimize))
      declaration-identifier-cst
      declaration-data)
-  (loop for data = declaration-data then (rest data)
-        until (null data)
-        collect (list declaration-identifier-cst data)))
+  (map-prefix declaration-identifier-cst declaration-data))
