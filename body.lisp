1881f1eccef10bf536f59a3f53f89391c03a3936
Robert Strandh <robert.strandh@gmail.com>
2017-04-26 18:26:59 +0200

    Define method on generic function SEPARATE-FUNCTION-BODY.

--- body.lisp
+++ body.lisp
@@ -9,3 +9,21 @@
         do (push (first remaining) declarations)
         finally (return (values (reverse declarations)
                                 (listify remaining)))))
+
+(defmethod separate-function-body ((body cons-cst))
+  (loop with declarations = '()
+        with documentation = nil
+        for remaining = body then (rest remaining)
+        until (or (typep remaining 'null-cst)
+                  (and (atom (first remaining))
+                       (not (stringp (raw (first remaining)))))
+                  (and (stringp (raw (first remaining)))
+                       (not (null documentation)))
+                  (and (consp (first remaining))
+                       (not (eq (raw (first (first remaining))) 'declare))))
+        do (if (stringp (raw (first remaining)))
+               (setf documentation (first remaining))
+               (push (first remaining) declarations))
+        finally (return (values (reverse declarations)
+                                documentation
+                                (listify remaining)))))
