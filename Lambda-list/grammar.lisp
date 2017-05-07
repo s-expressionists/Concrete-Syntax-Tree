593f0a3a57ff2b68b6e4801c837dbd31dad86567
Robert Strandh <robert.strandh@gmail.com>
2017-05-07 09:09:49 +0200

    Define class RULE.

--- Lambda-list/grammar.lisp
+++ Lambda-list/grammar.lisp
@@ -3,3 +3,7 @@
 (defclass grammar ()
   ((%rules :initarg :rules :reader rules)
    (%nullable-symbols :reader nullable-symbols)))
+
+(defclass rule ()
+  ((&left-hand-slide :initarg :left-hand-side :reader left-hand-side)
+   (&right-hand-slide :initarg :right-hand-side :reader right-hand-side)))
