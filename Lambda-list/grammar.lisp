cb622edd1c275d09cf957ec3deed15df999764b8
Robert Strandh <robert.strandh@gmail.com>
2017-05-07 09:10:21 +0200

    Define :AFTER method on INITIALIZE-INSTANCE to turn rules into standard instances.

--- Lambda-list/grammar.lisp
+++ Lambda-list/grammar.lisp
@@ -7,3 +7,11 @@
 (defclass rule ()
   ((&left-hand-slide :initarg :left-hand-side :reader left-hand-side)
    (&right-hand-slide :initarg :right-hand-side :reader right-hand-side)))
+
+(defmethod initialize-instance :after ((object grammar) &key rules)
+  (reinitialize-instance
+   object
+   :rules (loop for rule in rules
+                collect (make-instance 'rule
+                          :left-hand-side (car rule)
+                          :right-hand-side (cddr rule)))))
