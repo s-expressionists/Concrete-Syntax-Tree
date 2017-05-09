c157420b82bec0f13e910fefe0f233effafae6d4
Robert Strandh <robert.strandh@gmail.com>
2017-05-09 18:02:57 +0200

    Define function for computing all symbols in a grammar.

--- Lambda-list/grammar.lisp
+++ Lambda-list/grammar.lisp
@@ -21,3 +21,11 @@
       (list right-hand-side-element)
       (loop for element in (cdr right-hand-side-element)
             append (extract-symbols element))))
+
+(defun compute-all-symbols (grammar)
+  (let ((non-nullable (make-hash-table :test #'eq)))
+    (loop for rule in (rules grammar)
+          do (setf (gethash (left-hand-side rule) non-nullable) t)
+             (loop for element in (right-hand-side rule)
+                   do (loop for symbol in (extract-symbols element)
+                            do (setf (gethash symbol non-nullable) t))))))
