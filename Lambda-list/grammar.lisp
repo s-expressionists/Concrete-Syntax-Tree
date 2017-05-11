6ac763ac9b38f901d0ce3303070eace3dca94847
Robert Strandh <robert.strandh@gmail.com>
2017-05-11 08:49:40 +0200

    Define :INITARG for slot NULLABLE-SYMBOLS.

--- Lambda-list/grammar.lisp
+++ Lambda-list/grammar.lisp
@@ -2,7 +2,7 @@
 
 (defclass grammar ()
   ((%rules :initarg :rules :reader rules)
-   (%nullable-symbols :reader nullable-symbols)))
+   (%nullable-symbols :initarg :nullable-symbols :reader nullable-symbols)))
 
 (defclass rule ()
   ((&left-hand-slide :initarg :left-hand-side :reader left-hand-side)
