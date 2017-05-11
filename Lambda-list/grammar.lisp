c27a7f3a18f8fc305a9e5db39d3cad417c6fd60b
Robert Strandh <robert.strandh@gmail.com>
2017-05-11 08:48:38 +0200

    Move :AFTER method on INITIALIZE-INSTANCE to the end of the file.

--- Lambda-list/grammar.lisp
+++ Lambda-list/grammar.lisp
@@ -8,14 +8,6 @@
   ((&left-hand-slide :initarg :left-hand-side :reader left-hand-side)
    (&right-hand-slide :initarg :right-hand-side :reader right-hand-side)))
 
-(defmethod initialize-instance :after ((object grammar) &key rules)
-  (reinitialize-instance
-   object
-   :rules (loop for rule in rules
-                collect (make-instance 'rule
-                          :left-hand-side (car rule)
-                          :right-hand-side (cddr rule)))))
-
 (defun extract-symbols (right-hand-side-element)
   (if (symbolp right-hand-side-element)
       (list right-hand-side-element)
@@ -50,3 +42,11 @@
                                 t)
                           (setf modified-p t)))))
     nullable-symbols))
+
+(defmethod initialize-instance :after ((object grammar) &key rules)
+  (reinitialize-instance
+   object
+   :rules (loop for rule in rules
+                collect (make-instance 'rule
+                          :left-hand-side (car rule)
+                          :right-hand-side (cddr rule)))))
