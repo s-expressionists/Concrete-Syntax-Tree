a713dd243abacba96a6ff2c365606ea1e9db7157
Robert Strandh <robert.strandh@gmail.com>
2017-05-17 10:04:55 +0200

    Define generic function for adding an item to a state.

--- Lambda-list/earley.lisp
+++ Lambda-list/earley.lisp
@@ -20,6 +20,13 @@
 (defclass earley-state ()
   ((%items :initform '() :accessor items)))
 
+(defgeneric possibly-add-item (item state))
+
+(defmethod possibly-add-item ((item earley-item) (state earley-state))
+  (unless (find item (items state) :test #'item-equal)
+    (setf (items state)
+          (nconc (items state) (list item)))))
+
 (defgeneric scanner-action
     (client item lambda-list terminal input))
 
