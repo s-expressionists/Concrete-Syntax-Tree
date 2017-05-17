e878d5d6b2086bf2ce2d79bd23cd47173c8080f7
Robert Strandh <robert.strandh@gmail.com>
2017-05-17 09:36:13 +0200

    New component containing code for the Earley parser.

---
+++ Lambda-list/earley.lisp
@@ -0,0 +1,32 @@
+(cl:in-package #:concrete-syntax-tree)
+
+(defclass rule ()
+  ((%left-hand-side :initarg :left-hand-side :reader left-hand-side)
+   (%right-hand-side :initarg :right-hand-side :reader right-hand-side)))
+
+(defclass earley-item ()
+  ((%rule :initarg :rule :reader rule)
+   (%dot-position :initarg :dot-position :reader dot-position)
+   (%parse-trees :initarg :parse-trees :reader parse-trees)))
+
+(defclass earley-state ()
+  ((%items :initform '() :accessor items)))
+
+(defgeneric scanner-action
+    (client item lambda-list terminal input))
+
+(defclass grammar-symbol ()
+  ((%parse-tree :initarg :parse-tree :reader parse-tree)))
+
+(defclass simple-variable (grammar-symbol) ())
+
+(defmethod scanner-action
+    (client item lambda-list (terminal simple-variable) input)
+  (if (symbolp input)
+      (make-instance 'earley-item
+        :rule (rule item)
+        :parse-trees (cons (parse-trees item)
+                           (make-instance 'simple-variable
+                             :parse-tree input))
+        :dot-position (1+ (dot-position item)))
+      nil))
