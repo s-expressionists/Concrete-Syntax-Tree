a5479482ffbf67c2c8837f020620bfd48e2a238b
Robert Strandh <robert.strandh@gmail.com>
2017-05-17 10:02:40 +0200

    Define generic function for comparing two items for equality.

--- Lambda-list/earley.lisp
+++ Lambda-list/earley.lisp
@@ -10,6 +10,13 @@
    (%origin :initarg :origin :reader origin)
    (%parse-trees :initarg :parse-trees :reader parse-trees)))
 
+(defgeneric item-equal (item1 item2))
+
+(defmethod item-equal ((item1 earley-item) (item2 earley-item))
+  (and (eq (rule item1) (rule item2))
+       (eq (dot-position item1) (dot-position item2))
+       (eq (origin item1) (origin item2))))
+
 (defclass earley-state ()
   ((%items :initform '() :accessor items)))
 
