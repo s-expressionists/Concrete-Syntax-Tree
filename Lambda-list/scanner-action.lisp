660e250f487b6e3c6f411f1efb381fe33ba0d250
Robert Strandh <robert.strandh@gmail.com>
2017-05-19 15:42:28 +0200

    Define method on SCANNER-ACTION for KEYWORD-OPTIONAL.

--- Lambda-list/scanner-action.lisp
+++ Lambda-list/scanner-action.lisp
@@ -19,3 +19,13 @@
          (make-instance 'ordinary-required-parameter
            :parse-tree input))
         nil)))
+
+(defmethod scanner-action
+    (client item lambda-list (terminal keyword-optional) input)
+  (declare (ignore client lambda-list))
+  (if (eq input '&optional)
+      (advance-dot-position
+       item
+       (make-instance 'keyword-optional
+         :parse-tree input))
+      nil))
