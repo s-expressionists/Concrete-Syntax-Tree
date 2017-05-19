3b7f9521793e88863ba5f34a50309817bf9bc8f0
Robert Strandh <robert.strandh@gmail.com>
2017-05-19 11:06:12 +0200

    Push initial item on first state.

--- Lambda-list/earley.lisp
+++ Lambda-list/earley.lisp
@@ -82,11 +82,20 @@
    (%remaining-input :initarg :input :accessor remaining-input)))
 
 (defmethod initialize-instance :after ((object parser) &key rules)
-  (reinitialize-instance
-   object
-   :states (loop repeat (1+ (length (all-input object)))
-                 collect (make-instance 'earley-state))
-   :grammar (make-instance 'grammar :rules rules)))
+  (let* ((states (loop repeat (1+ (length (all-input object)))
+                       collect (make-instance 'earley-state)))
+         (grammar (make-instance 'grammar :rules rules))
+         (target-rule (find 'target (rules grammar) :key #'left-hand-side))
+         (item (make-instance 'earley-item
+                 :parse-trees '()
+                 :origin (car states)
+                 :dot-position 0
+                 :rule target-rule)))
+    (push item (car states))
+    (reinitialize-instance
+     object
+     :states states
+     :grammar grammar)))
 
 (defgeneric process-current-state (parser))
 
