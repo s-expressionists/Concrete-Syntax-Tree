eb2f974c859639767c5c2491174009dfee751cb1
Robert Strandh <robert.strandh@gmail.com>
2017-05-22 08:16:49 +0200

    Supply earmuffs on special variables.

--- Lambda-list/standard-grammars.lisp
+++ Lambda-list/standard-grammars.lisp
@@ -1,31 +1,31 @@
 (cl:in-package #:concrete-syntax-tree)
 
-(defparameter ordinary-required-parameters
+(defparameter *ordinary-required-parameters*
   '((ordinary-required-parameters <-
      (* ordinary-required-parameter))))
 
-(defparameter ordinay-optional-parameters
+(defparameter *ordinay-optional-parameters*
   '((ordinary-optional-parameters <-
      keyword-optional
      (* ordinary-optional-parameter))))
 
-(defparameter rest-parameter
+(defparameter *rest-parameter*
   '((rest-parameter <-
      keyword-rest
      simple-variable)))
 
-(defparameter ordinary-key-parameters
+(defparameter *ordinary-key-parameters*
   '((ordinary-key-parameters <-
      keyword-key
      (* ordinary-key-parameter)
      (? key-allow-other-keys))))
 
-(defparameter aux-parameters
+(defparameter *aux-parameters*
   '((aux-parameters <-
      keyword-aux
      (* aux-parameter))))
 
-(defparameter ordinary-lambda-list
+(defparameter *ordinary-lambda-list*
   '((lambda-list <-
      (? ordinary-required-parameters)
      (? ordinary-optional-parameters)
@@ -33,29 +33,29 @@
      (? ordinary-key-parameters)
      (? aux-parameters))))
 
-(defparameter generic-function-optional-parameters
+(defparameter *generic-function-optional-parameters*
   '((generic-function-optional-parameters <-
      keyword-optional
      (* generic-function-optional-parameter))))
 
-(defparameter generic-function-key-parameters
+(defparameter *generic-function-key-parameters*
   '((generic-function-key-parameters <-
      keyword-key
      (* generic-function-key-parameter)
      (? key-allow-other-keys))))
 
-(defparameter generic-function-lambda-list
+(defparameter *generic-function-lambda-list*
   '((lambda-list <-
      (? ordinary-required-parameters)
      (? generic-function-optional-parameters)
      (? rest-parameter)
      (? generic-function-key-parameters))))
 
-(defparameter specialized-required-parameters
+(defparameter *specialized-required-parameters*
   '((required-parameters <-
      (* specialized-required-parameter))))
 
-(defparameter specialized-lambda-list
+(defparameter *specialized-lambda-list*
   '((lambda-list <-
      (? required-parameters)
      (? optional-parameters)
@@ -63,12 +63,12 @@
      (? key-parameters)
      (? aux-parameters))))
 
-(defparameter environment-parameter
+(defparameter *environment-parameter*
   '((environment-parameter <-
      keyword-environment
      simple-variable)))
 
-(defparameter whole-parameter
+(defparameter *whole-parameter*
   '((whole-parameter <-
      keyword-whole
      simple-variable)))
