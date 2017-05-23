eb5c5af9e3e4b67bbe963b2e6d37aabea7ed5f87
Robert Strandh <robert.strandh@gmail.com>
2017-05-23 09:50:45 +0200

    Define method on PRINT-OBJECT specialized to EARLEY-ITEM.

--- Lambda-list/earley-item.lisp
+++ Lambda-list/earley-item.lisp
@@ -6,6 +6,20 @@
    (%origin :initarg :origin :reader origin)
    (%parse-trees :initarg :parse-trees :reader parse-trees)))
 
+(defmethod print-object ((object earley-item) stream)
+  (let ((rule (rule object))
+        (pos (dot-position object)))
+    (print-unreadable-object (rule stream :type t)
+      (format stream "~s <- " (left-hand-side rule))
+      (loop for symbol in (right-hand-side rule)
+            for i from 0
+            do (when (= pos i)
+                 (format stream " . "))
+               (format stream "~s " symbol))
+      (when (= pos (length (right-hand-side rule)))
+        (format stream " . "))
+      (terpri stream))))
+
 (defgeneric item-equal (item1 item2))
 
 (defmethod item-equal ((item1 earley-item) (item2 earley-item))
