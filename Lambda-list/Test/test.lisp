6a090311016a3ae7b7f6d563756a1342d0e72472
Robert Strandh <robert.strandh@gmail.com>
2017-05-26 10:00:21 +0200

    Define function for asserting parse success.

--- Lambda-list/Test/test.lisp
+++ Lambda-list/Test/test.lisp
@@ -1,5 +1,14 @@
 (cl:in-package #:concrete-syntax-tree-lambda-list-test)
 
+(defun assert-success (parser)
+  (let ((initial-state (car (cst::all-states parser)))
+        (final-state (car (cl:last (cst::all-states parser)))))
+    (assert (find-if (lambda (item)
+                       (and (eq (cst::left-hand-side (cst::rule item))
+                                'cst::target)
+                            (eq (cst::origin item) initial-state)))
+                     (cst::items final-state)))))
+
 (defun test1 ()
   (let ((p (make-instance 'cst::parser
              :rules cst::*ordinary-lambda-list-grammar*
