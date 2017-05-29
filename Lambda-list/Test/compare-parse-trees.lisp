13b0429bf3c4c257e157f201cfa3005a1343d55d
Robert Strandh <robert.strandh@gmail.com>
2017-05-29 10:15:30 +0200

    Replace COMPARE-CHILDREN with COMPARE-LIST.

--- Lambda-list/Test/compare-parse-trees.lisp
+++ Lambda-list/Test/compare-parse-trees.lisp
@@ -6,19 +6,25 @@
   (declare (ignore tree1 tree2))
   nil)
 
-(defun compare-children (tree1 tree2)
-  (and (eq (class-of tree1) (class-of tree2))
-       (every #'compare-parse-trees
-              (cst::children tree1)
-              (cst::children tree2))))
-   
-(defmethod compare-parse-trees
-    ((tree1 cst::lambda-list-type) (tree2 cst::lambda-list-type))
-  (compare-children tree1 tree2))
+(defun compare-lists (list1 list2)
+  (and (= (length list1) (length list2))
+       (every #'compare-parse-trees list1 list2)))
 
 (defmethod compare-parse-trees
-    ((tree1 cst::parameter-group) (tree2 cst::parameter-group))
-  (compare-children tree1 tree2))
+    ((tree1 cst::lambda-list-type) (tree2 cst::lambda-list-type))
+  (and (eq (class-of tree1) (class-of tree2))
+       (compare-lists (cst::children tree1) (cst::children tree2))))
+
+(defmethod compare-parse-trees
+    ((tree1 cst::implicit-parameter-group)
+     (tree2 cst::implicit-parameter-group))
+  (compare-lists (cst::parameters tree1) (cst::parameters tree2)))
+
+(defmethod compare-parse-trees
+    ((tree1 cst::explicit-parameter-group)
+     (tree2 cst::explicit-parameter-group))
+  (and (eq (cst::keyword tree1) (cst::keyword tree2))
+       (compare-lists (cst::parameters tree1) (cst::parameters tree2))))
 
 (defmethod compare-parse-trees
     ((tree1 cst::ordinary-required-parameter)
