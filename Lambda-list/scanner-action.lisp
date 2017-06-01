cbc1ec58fa95d71a82e7cdd87e934318e0275e83
Robert Strandh <robert.strandh@gmail.com>
2017-06-01 10:18:02 +0200

    Associate class KEYWORD-ALLOW-OTHER-KEYS with keyword &ALLOW-OTHER-KEYS.

--- Lambda-list/scanner-action.lisp
+++ Lambda-list/scanner-action.lisp
@@ -159,7 +159,7 @@
 (define-keyword-scanner-action keyword-rest &rest)
 (define-keyword-scanner-action keyword-body &rest)
 (define-keyword-scanner-action keyword-key &key)
-(define-keyword-scanner-action keyword-key &allow-other-keys)
+(define-keyword-scanner-action keyword-allow-other-keys &allow-other-keys)
 (define-keyword-scanner-action keyword-aux &aux)
 (define-keyword-scanner-action keyword-environment &environment)
 (define-keyword-scanner-action keyword-whole &whole)
