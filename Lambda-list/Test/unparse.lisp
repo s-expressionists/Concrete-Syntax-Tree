e24ce0febceb9895cef19fdb663a0b1b7af36f62
Robert Strandh <robert.strandh@gmail.com>
2017-06-20 10:42:06 +0200

    Add remaining methods on UNPARSE.

--- Lambda-list/Test/unparse.lisp
+++ Lambda-list/Test/unparse.lisp
@@ -10,3 +10,29 @@
 
 (defmethod unparse ((tree cst::lambda-list-keyword))
   (cst::name tree))
+
+(defmethod unparse ((tree cst::simple-variable))
+  (cst::name tree))
+
+(defmethod unparse ((tree cst::ordinary-optional-parameter))
+  (list (cst::name tree)
+        (cst::form tree)
+        (cst::supplied-p tree)))
+
+(defmethod unparse ((tree cst::ordinary-key-parameter))
+  (list (list (cst::keyword tree) (cst::name tree))
+        (cst::form tree)
+        (cst::supplied-p tree)))
+
+(defmethod unparse ((tree cst::aux-parameter))
+  (list (cst::name tree)
+        (cst::form tree)))
+
+(defmethod unparse ((tree cst::generic-function-optional-parameter))
+  (cst::name tree))
+
+(defmethod unparse ((tree cst::generic-function-key-parameter))
+  (list (list (cst::keyword tree) (cst::name tree))))
+
+(defmethod unparse ((tree cst::specialized-required-parameter))
+  (list (cst::name tree) (cst::specializer tree)))
