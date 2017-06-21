9172fb7fae8b2de377c86ed0878bb1816fccc8bd
Robert Strandh <robert.strandh@gmail.com>
2017-06-21 11:30:18 +0200

    Compare raw versions of parameter components.

--- Lambda-list/Test/compare-parse-trees.lisp
+++ Lambda-list/Test/compare-parse-trees.lisp
@@ -25,44 +25,54 @@
     ((tree1 cst::explicit-parameter-group)
      (tree2 cst::explicit-parameter-group))
   (and (eq (class-of tree1) (class-of tree2))
-       (eq (cst::name (cst::keyword tree1)) (cst::name (cst::keyword tree2)))
+       (eq (cst:raw (cst::name (cst::keyword tree1)))
+           (cst:raw (cst::name (cst::keyword tree2))))
        (compare-lists (cst::parameters tree1) (cst::parameters tree2))))
 
 (defmethod compare-parse-trees
     ((tree1 cst::singleton-parameter-group)
      (tree2 cst::singleton-parameter-group))
   (and (eq (class-of tree1) (class-of tree2))
-       (eq (cst::name (cst::keyword tree1)) (cst::name (cst::keyword tree2)))
+       (eq (cst:raw (cst::name (cst::keyword tree1)))
+           (cst:raw (cst::name (cst::keyword tree2))))
        (compare-parse-trees (cst::parameter tree1) (cst::parameter tree2))))
 
 (defmethod compare-parse-trees
     ((tree1 cst::simple-variable)
      (tree2 cst::simple-variable))
-  (eq (cst::name tree1) (cst::name tree2)))
+  (eq (cst:raw (cst::name tree1)) (cst:raw (cst::name tree2))))
 
 (defmethod compare-parse-trees
     ((tree1 cst::ordinary-optional-parameter)
      (tree2 cst::ordinary-optional-parameter))
-  (and (eq (cst::name tree1) (cst::name tree2))
-       (equal (cst::form tree1) (cst::form tree2))
-       (or (and (null (symbol-package (cst::supplied-p tree1)))
-                (null (symbol-package (cst::supplied-p tree2))))
-           (eq (cst::supplied-p tree1) (cst::supplied-p tree2)))))
+  (and (eq (cst:raw (cst::name tree1))
+           (cst:raw (cst::name tree2)))
+       (equal (cst:raw (cst::form tree1))
+              (cst:raw (cst::form tree2)))
+       (or (and (null (symbol-package (cst:raw (cst::supplied-p tree1))))
+                (null (symbol-package (cst:raw (cst::supplied-p tree2)))))
+           (eq (cst:raw (cst::supplied-p tree1))
+               (cst:raw (cst::supplied-p tree2))))))
 
 (defmethod compare-parse-trees
     ((tree1 cst::generic-function-optional-parameter)
      (tree2 cst::generic-function-optional-parameter))
-  (eq (cst::name tree1) (cst::name tree2)))
+  (eq (cst:raw (cst::name tree1))
+      (cst:raw (cst::name tree2))))
 
 (defmethod compare-parse-trees
     ((tree1 cst::ordinary-key-parameter)
      (tree2 cst::ordinary-key-parameter))
-  (and (eq (cst::name tree1) (cst::name tree2))
-       (eq (cst::keyword tree1) (cst::keyword tree2))
-       (equal (cst::form tree1) (cst::form tree2))
-       (or (and (null (symbol-package (cst::supplied-p tree1)))
-                (null (symbol-package (cst::supplied-p tree2))))
-           (eq (cst::supplied-p tree1) (cst::supplied-p tree2)))))
+  (and (eq (cst:raw (cst::name tree1))
+           (cst:raw (cst::name tree2)))
+       (eq (cst:raw (cst::keyword tree1))
+           (cst:raw (cst::keyword tree2)))
+       (equal (cst:raw (cst::form tree1))
+              (cst:raw (cst::form tree2)))
+       (or (and (null (symbol-package (cst:raw (cst::supplied-p tree1))))
+                (null (symbol-package (cst:raw (cst::supplied-p tree2)))))
+           (eq (cst:raw (cst::supplied-p tree1))
+               (cst:raw (cst::supplied-p tree2))))))
 
 (defmethod compare-parse-trees
     ((tree1 cst::generic-function-key-parameter)
@@ -73,8 +83,10 @@
 (defmethod compare-parse-trees
     ((tree1 cst::aux-parameter)
      (tree2 cst::aux-parameter))
-  (and (eq (cst::name tree1) (cst::name tree2))
-       (equal (cst::form tree1) (cst::form tree2))))
+  (and (eq (cst:raw (cst::name tree1))
+           (cst:raw (cst::name tree2)))
+       (equal (cst:raw (cst::form tree1))
+              (cst:raw (cst::form tree2)))))
 
 (defmethod compare-parse-trees
     ((tree1 cst::specialized-required-parameter)
