32559bd8194eb0902ca2812bdf43f49a3ded4404
Robert Strandh <robert.strandh@gmail.com>
2017-07-19 06:47:25 +0200

    Define a function for checking whether a binding has a valid shape.

--- bindings.lisp
+++ bindings.lisp
@@ -5,3 +5,13 @@
 ;;;; such a binding can have three different shapes, namely VAR,
 ;;;; (VAR), or (VAR INIT-FORM).  The last of these three shapes is
 ;;;; considered canonical.
+
+;;; Check whether a binding is valid, i.e., has one of the three valid
+;;; shapes.
+(defun valid-binding-p (binding-cst)
+  (let ((raw (raw binding-cst)))
+    (or (symbolp raw)
+        (and (cl:consp raw)
+             (or (cl:null (cdr raw))
+                 (and (cl:consp (cdr raw))
+                      (cl:null (cddr raw))))))))
