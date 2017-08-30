1fd972a0cb74533bfcbfeee7675e56a8962d69d7
Robert Strandh <robert.strandh@gmail.com>
2017-08-30 17:22:32 +0200

    Simplify CST-FROM-EXPRESSION by not taking into account sharing.

--- cst-from-expression.lisp
+++ cst-from-expression.lisp
@@ -1,39 +1,21 @@
 (cl:in-package #:concrete-syntax-tree)
 
-(defvar *table*)
-
 (defun cst-from-element (expression)
-  (let ((existing (gethash expression *table*)))
-    (cond ((not (cl:null existing))
-           existing)
-          ((cl:atom expression)
-           (setf (gethash expression *table*)
-                 (make-instance 'atom-cst
-                   :raw expression)))
-          (t
-           (cst-from-list-expression expression)))))
+  (cond ((cl:atom expression)
+         (make-instance 'atom-cst
+           :raw expression))
+        (t
+         (cst-from-list-expression expression))))
 
 (defun cst-from-list-expression (expression)
-  (let ((existing (gethash expression *table*)))
-    (cond ((not (cl:null existing))
-           existing)
-          ((cl:null expression)
-           (make-instance 'atom-cst :raw nil))
-          ((cl:atom expression)
-           (setf (gethash expression *table*)
-                 (make-instance 'atom-cst
-                   :raw expression)))
-          (t
-           (let ((new (make-instance 'cons-cst
-                        :raw expression)))
-             (setf (gethash expression *table*) new)
-             (reinitialize-instance
-              new
-              :raw expression
-              :first (cst-from-element (car expression))
-              :rest (cst-from-list-expression (cdr expression)))
-             new)))))
+  (cond ((cl:atom expression)
+         (make-instance 'atom-cst
+           :raw expression))
+        (t
+         (make-instance 'cons-cst
+           :raw expression
+           :first (cst-from-element (car expression))
+           :rest (cst-from-list-expression (cdr expression))))))
 
 (defun cst-from-expression (expression)
-  (let ((*table* (make-hash-table :test #'eq)))
-    (cst-from-element expression)))
+  (cst-from-element expression))
