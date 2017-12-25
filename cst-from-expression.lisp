8d052d02764efe7650c4e62f85bcd5f4b24477d2
Robert Strandh <robert.strandh@gmail.com>
2017-12-25 15:36:09 +0100

    Handle self-referencing expressions.

--- cst-from-expression.lisp
+++ cst-from-expression.lisp
@@ -1,5 +1,7 @@
 (cl:in-package #:concrete-syntax-tree)
 
+(defvar *table*)
+
 (defun cst-from-element (expression)
   (cond ((cl:atom expression)
          (make-instance 'atom-cst
@@ -8,14 +10,18 @@
          (cst-from-list-expression expression))))
 
 (defun cst-from-list-expression (expression)
-  (cond ((cl:atom expression)
+  (cond ((nth-value 1 (gethash expression *table*))
+         (gethash expression *table*))
+        ((cl:atom expression)
          (make-instance 'atom-cst
            :raw expression))
         (t
-         (make-instance 'cons-cst
-           :raw expression
-           :first (cst-from-element (car expression))
-           :rest (cst-from-list-expression (cdr expression))))))
+         (let ((result (make-instance 'cons-cst :raw expression)))
+           (setf (gethash expression *table*) result)
+           (reinitialize-instance result
+              :first (cst-from-element (car expression))
+              :rest (cst-from-list-expression (cdr expression)))))))
 
 (defun cst-from-expression (expression)
-  (cst-from-element expression))
+  (let ((*table* (make-hash-table :test #'eq)))
+    (cst-from-element expression)))
