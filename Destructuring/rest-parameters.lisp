4fee4394c0e91e8c1f769d1120ca5673ec9b2bf0
Bike <aeshtaer@gmail.com>
2020-03-24 14:47:03 -0400

    Support destructuring &rest (fix #24)

--- Destructuring/rest-parameters.lisp
+++ Destructuring/rest-parameters.lisp
@@ -4,6 +4,10 @@
     (client (parameter simple-variable) argument-variable)
   `((,(raw (name parameter)) ,argument-variable)))
 
+(defmethod rest-parameter-bindings
+    (client (parameter destructuring-lambda-list) argument-variable)
+  (destructuring-lambda-list-bindings client parameter argument-variable))
+
 (defmethod parameter-group-bindings
     (client (parameter-group destructuring-rest-parameter-group)
      argument-variable)
