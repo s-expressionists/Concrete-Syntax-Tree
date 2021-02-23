ebfd20f761fe8b95519691a35de278214312f27b
Bike <aeshtaer@gmail.com>
2021-02-23 09:38:35 -0500

    Have parse-macro accept and return CSTs
    But with no source info, just yet
    
    Note that this requires a corresponding change from Cleavir.

--- Destructuring/parse-macro.lisp
+++ Destructuring/parse-macro.lisp
@@ -33,28 +33,29 @@
     (multiple-value-bind (bindings ignorables)
         (destructuring-lambda-list-bindings
          client relevant-lambda-list args-var)
-      `(lambda (,final-form-var ,final-env-var)
-         (block ,(raw name)
-           (let* ((,args-var (cdr ,final-form-var))
-                  ,@bindings
-                  ;; We rebind the whole and environment variables
-                  ;; here, so that any user declarations for them
-                  ;; are scoped, properly.
-                  ;; We do this AFTER the args-var binding so that
-                  ;; if, e.g., a &whole is declared ignore, the
-                  ;; compiler does not complain that it was used
-                  ;; for the args-var binding.
-                  ,@(if (cl:null form-var)
-                        `()
-                        `((,final-form-var ,final-form-var)))
-                  (,final-env-var ,final-env-var))
-             (declare (ignorable ,@ignorables)
-                      ;; If the lambda list does not contain &environment, then
-                      ;; we IGNORE the GENSYMed parameter to avoid warnings.
-                      ;; If the lambda list does contain &environment, we do
-                      ;; not want to make it IGNORABLE because we would want a
-                      ;; warning if it is not used then.
-                      ,@(if (cl:null env-var)
-                            `((ignore ,final-env-var))
-                            `()))
-             ,@body))))))
+      (cst-from-expression
+       `(lambda (,final-form-var ,final-env-var)
+          (block ,(raw name)
+            (let* ((,args-var (cdr ,final-form-var))
+                   ,@bindings
+                   ;; We rebind the whole and environment variables
+                   ;; here, so that any user declarations for them
+                   ;; are scoped, properly.
+                   ;; We do this AFTER the args-var binding so that
+                   ;; if, e.g., a &whole is declared ignore, the
+                   ;; compiler does not complain that it was used
+                   ;; for the args-var binding.
+                   ,@(if (cl:null form-var)
+                         `()
+                         `((,final-form-var ,final-form-var)))
+                   (,final-env-var ,final-env-var))
+              (declare (ignorable ,@ignorables)
+                       ;; If the lambda list does not contain &environment, then
+                       ;; we IGNORE the GENSYMed parameter to avoid warnings.
+                       ;; If the lambda list does contain &environment, we do
+                       ;; not want to make it IGNORABLE because we would want a
+                       ;; warning if it is not used then.
+                       ,@(if (cl:null env-var)
+                             `((ignore ,final-env-var))
+                             `()))
+              ,@(raw body))))))))
