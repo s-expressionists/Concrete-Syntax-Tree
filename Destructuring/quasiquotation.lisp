e71d33bfac6ea06c13b99245e99c02bed6e7e4ae
Bike <aeshtaer@gmail.com>
2021-02-23 13:36:32 -0500

    Reduce quasiquotations more aggressively

--- Destructuring/quasiquotation.lisp
+++ Destructuring/quasiquotation.lisp
@@ -2,6 +2,15 @@
 
 (defun cst-append (&rest csts) (cstify (mapcan #'listify csts)))
 
+(defun cst-list* (first &rest rest)
+  (if (cl:null rest)
+      first
+      (cons first (apply #'cst-list* rest))))
+
+(defun needs-append-p (expr)
+  (and (cl:consp expr)
+       (eq (cl:car expr) 'unquote-splicing)))
+
 (defun expand-quasiquotation (sourcef argument)
   (if (cl:atom argument)
       `(make-instance 'atom-cst :raw ',argument :source ,sourcef)
@@ -12,24 +21,23 @@
          (expand-quasiquoted-list sourcef argument)))))
 
 (defun expand-quasiquoted-list (sourcef list)
-  (if (loop for expr in list
-            always (or (cl:atom expr)
-                       (not (eq (cl:car expr) 'unquote-splicing))))
-      `(list ,@(loop for expr in list
-                     collect (if (cl:atom expr)
-                                 `(make-instance 'atom-cst
-                                    :raw ',expr :source ,sourcef)
-                                 (expand-quasiquotation sourcef expr))))
-      `(cst-append
-        ,@(loop for expr in list
-                collect (cond ((cl:atom expr)
-                               `(list (make-instance 'atom-cst
-                                        :raw ',expr :source ,sourcef)))
-                              ((eq (cl:car expr) 'unquote-splicing)
-                               (cl:second expr))
-                              (t
-                               `(list
-                                 ,(expand-quasiquotation sourcef expr))))))))
+  (cond ((notany #'needs-append-p list)
+         `(list ,@(loop for expr in list
+                        collect (expand-quasiquotation sourcef expr))))
+        ((notany #'needs-append-p (butlast list))
+         `(cst-list* ,@(loop for expr in (butlast list)
+                             collect (expand-quasiquotation sourcef expr))
+                     ;; The last must be an unquote-splicing, so:
+                     ,(cl:second (cl:first (last list)))))
+        (t ; full append
+         `(cst-append
+           ,@(loop for expr in list
+                   collect (cond
+                             ((eq (cl:car expr) 'unquote-splicing)
+                              (cl:second expr))
+                             (t
+                              `(list
+                                ,(expand-quasiquotation sourcef expr)))))))))
 
 (defmacro quasiquote (sourcef argument)
   (expand-quasiquotation sourcef argument))
