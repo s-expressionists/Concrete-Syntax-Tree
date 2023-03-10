422ef4797f9d0978fa6b5be2ec319b083e070786
Paul A. Patience <paul@apatience.com>
2023-03-10 04:22:59 -0500

    Make client the first argument to RECONSTRUCT

--- Test/reconstruct.lisp
+++ Test/reconstruct.lisp
@@ -4,10 +4,10 @@
   (let ((circular (cst:cons (make-instance 'cst:atom-cst :raw nil)
                             (make-instance 'cst:atom-cst :raw nil))))
     (setf (slot-value circular 'cst::%first) circular)
-    (cst:reconstruct '#1=(#1#) circular nil)))
+    (cst:reconstruct nil '#1=(#1#) circular)))
 
 (defun test-reconstruct-1 ()
   (let* ((cons (cons 'a 'c))
          (thing (cst:cst-from-expression cons)))
-    (assert (eq (cst:first (cst:rest (cst:reconstruct (list 'b cons) thing nil)))
+    (assert (eq (cst:first (cst:rest (cst:reconstruct nil (list 'b cons) thing)))
                 thing))))
